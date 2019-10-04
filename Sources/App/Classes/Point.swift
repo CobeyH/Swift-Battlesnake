bf6755938ca24b3403d1b1cf4735d13294b0392b
Cobey Hollier <cobey.hollier@mac.com>
2019-10-03 22:03:58 -0700

    CH: Started creating structs to decode data

--- Sources/App/Classes/Point.swift
+++ Sources/App/Classes/Point.swift
@@ -4,5 +4,20 @@
 //
 //  Created by Cobey Hollier on 2019-10-03.
 //
+import Vapor
 
 import Foundation
+
+final class Point: Content {
+    let x: Int
+    let y: Int
+
+    init(x: Int, y: Int) {
+        self.x = x
+        self.y = y
+    }
+    
+    func log() {
+        print("(\(self.x), \(self.y))")
+    }
+}
--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,4 +1,5 @@
 import Vapor
+import SwiftyJSON
 
 /// Register your application's routes here.
 public func routes(_ router: Router) throws {
@@ -14,7 +15,9 @@ public func routes(_ router: Router) throws {
     
     // Response to /move requests
     router.post("move") { req -> [String: String] in
+        print(req)
         let test = ["move": "left"]
+        let json = req.content
         return test
     }
     
@@ -25,5 +28,29 @@ public func routes(_ router: Router) throws {
     router.post("end") {req -> [String: String] in
         print("We Died")
         return ["move": "up"]
+    
     }
+}
+
+struct Data: Content {
+    let game: Game
+    let turn: Int
+    let board: Board
+}
+
+struct Game: Content {
+    let id: String
+}
+
+struct Board: Content {
+    let food: [Point]
+    let snakes: [Snake]
+    let you: Snake
+}
+
+struct Snake: Content {
+    let id: String
+    let name: String
+    let health: Int
+    let body: [Point]
 }
